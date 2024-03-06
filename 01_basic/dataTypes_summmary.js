6bac0511766233915218f1073d54b6cd562b4b16
Mayank Kumar <draxgaming19@gmail.com>
2024-03-06 13:11:28 +0000

    dataType revision

---
+++ 01_basic/dataTypes_summmary.js
@@ -0,0 +1,12 @@
+// -----> Primitive
+// |-> 7 types : string, Number, Boolean, NULL, undefined, Symbol, BigINT
+
+
+// |-> JavaScipt is a DYNAMICALLY typed language
+// |-> typeScript is a Static Typed language
+
+
+
+
+// ------>  Reference(Non PRIMITIVE)
+// Array, Objects, Functions
