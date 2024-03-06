9b240ed7caebbce527e52c580a0fa23ba8993ca1
Mayank Kumar <draxgaming19@gmail.com>
2024-03-06 04:48:18 +0000

    data_types

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 3870 // Can not change the value futher
+let accountEmail = "draxGaming@xyz.com"  
+let accoutPassword = "12345"
+accountCity = "Jaipur"
+let accountState; //undefined
+
+/*
+Prefer not to use var because of issue in block and functional scope
+*/
+
+console.table([accountId,accoutPassword,accountEmail,accountCity]);
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all JS Code as newer version
+
+// alert("") we are using nodeJs, not browser
+
+let name = "dRax"
+let age = 18
+let isLoggedIn = false
+
+// --------> primitiveDataTypes <------------
+
+//number-> 2^53
+//BigInt
+//string
+//boolean
+//null -> standalone value -> representation of empty value
+//undefined => the value is not assigned
+//symbol => unique
+
+//Objects
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
